a5a056b0640b1a80ad1d79d5176f63717f462dc0
Antonin <95379201+antoninpicard@users.noreply.github.com>
2026-03-31 15:36:27 +0200

    Add initial C++ version

---
+++ c.cpp
@@ -0,0 +1,16 @@
+/*
+    euh... version C++
+    encore plus inutile que la version C
+*/
+
+#include <iostream>
+
+int main() {
+    // euh... logique avancée
+    std::cout << "euh..." << std::endl;
+
+    // encore du remplissage inutile
+    // euh euh euh euh euh euh
+
+    return 0;
+}

e4bccbb9f6bb1b9a5fa3300bf97f4b0323142a55
Antonin <95379201+antoninpicard@users.noreply.github.com>
2026-03-31 15:35:37 +0200

    C program
    Add a C file with a simple program that prints a message.

---
+++ c.c
@@ -0,0 +1,28 @@
+/*
+    euh... bon voilà
+    ceci est un fichier C totalement inutile
+    mais qui existe pour des raisons très importantes (ou pas)
+
+    franchement y'a rien à voir ici
+    juste du texte pour faire genre
+
+    euh
+    euh
+    euh
+
+    merci de votre compréhension
+*/
+
+#include <stdio.h>
+
+int main() {
+    // euh... programme très complexe
+    // qui ne fait absolument rien
+
+    printf("euh...\n");
+
+    // encore du euh pour remplir
+    // euh euh euh euh euh
+
+    return 0;
+}
